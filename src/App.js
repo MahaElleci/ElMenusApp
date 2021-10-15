90325973f05b2897fa4e64e7a96f3f9db114c2ce
MahaElleci <maha.ehab.elleci@gmail.com>
2021-10-15 21:43:09 +0200

    added : missing packages in package.json, application routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router,  Switch,
+  Route } from 'react-router-dom';
+import LaunchPage from "./pages/LaunchPage"; 
+import LoginPage from "./pages/LoginPage"; 
+import AdminPage from './pages/AdminPage'; 
+import UsersPage from './pages/UsersPage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <Router>
+       <Switch>
+          <Route exact path="/">
+            <LaunchPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route> 
+          <Route path="/adminPage">
+            <AdminPage />
+          </Route> 
+          <Route path="/usersPage">
+            <UsersPage />
+          </Route> 
+        </Switch> 
+      </Router>
     </div>
   );
 }
